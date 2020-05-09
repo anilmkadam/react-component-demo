51c2e6304e6f4a48b5bcb9251d922134a84e10e7
anil kadam <anilkadam210@gmail.com>
2020-05-09 19:41:35 +0530

    Created simple blog post comment component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
-    return <div>Hello Folks!!!!</div>;
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 ReactDom.render(
