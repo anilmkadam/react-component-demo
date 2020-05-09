389e765577b4d6b7ea141d7751fc5ce2da022513
anil kadam <anilkadam210@gmail.com>
2020-05-09 22:35:28 +0530

    Iterate component using Comment Details data

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import faker from 'faker';
 
-const CommentDetail = () => {
+const CommentDetail = (prop) => {
+    let comment = prop.comment;
     return (
         <div className="comment">
             <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()} />
+                <img alt="avatar" src={comment.avatar} />
             </a>
             <div className="content">
                 <a href="/" className="author">
-                    Sam
+                    {comment.author || 'user'}
                 </a>
                 <div className="metadata">
-                    <span className="date">Today at 6:00PM</span>
+                    <span className="date">{comment._timestamp || '1 min ago'}</span>
                 </div>
-                <div className="text">Nice blog post</div>
+                    <div className="text">{comment.text || 'Cool'}</div>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,16 +1,52 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
+    let commentDetails = [
+        {
+            author: 'sam',
+            avatar: faker.image.avatar(),
+            _timestamp: 'Today at 6:00PM',
+            text: 'Nice Blog Post'
+        },
+        {
+            author: 'Alex',
+            avatar: faker.image.avatar(),
+            _timestamp: 'Yesterday at 12:00PM',
+            text: 'Informative'
+        },
+        {
+            author: 'Julie',
+            avatar: faker.image.avatar(),
+            _timestamp: 'Now',
+            text: 'Kudos to you'
+        },
+        {
+            author: 'Ryan',
+            avatar: faker.image.avatar(),
+            _timestamp: 'Today at 11:00AM',
+            text: 'Awesome'
+        },
+        {
+            author: 'Cris',
+            avatar: faker.image.avatar(),
+            _timestamp: '12 Min Ago',
+            text: 'Nice! Keep it up.'
+        },
+        {
+            avatar: faker.image.avatar(),
+        }
+    ];
+    var CommentDetailList = commentDetails.map((comment, i) => {
+                                return <CommentDetail comment={comment} key={i}/>
+                            });
     return (
         <div className="ui container comments">
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
+            {
+                CommentDetailList
+            }            
         </div>
     );
 }
